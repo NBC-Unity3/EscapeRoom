f7ab13f335d0f715c2d36c807430f7f5b4a0d3bd
sskesu <136444697+sskesu@users.noreply.github.com>
2024-02-14 15:33:29 +0900

    Feat : 과녁

--- Assets/Scripts/Puzzle/GunShoot.cs
+++ Assets/Scripts/Puzzle/GunShoot.cs
@@ -1,53 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class GunShoot : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
     [SerializeField] private GameObject pivot;
     [SerializeField] private ParticleSystem fireParticle;
+    Camera camera;
 
-    private bool onAim = false;
     private float range = 50f;
-
-    private Transform gameObj;
 
     private void Awake()
     {
-        gameObj = GetComponent<Transform>();
+        camera = Camera.main;
     }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Q))
-        {
-            onAim = true;
-        }
-        if (onAim)
-        {
-            OnAim();
-        }
         // 마우스 좌클릭(0)
         if (Input.GetMouseButtonDown(0))
         {
+            Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
             RaycastHit hit;
-            if (Physics.Raycast(player.transform.position, player.transform.forward, out hit, range))
+            if (Physics.Raycast(ray, out hit, range))
             {
-                Debug.Log(hit.collider.gameObject.name);
+                if (hit.collider.gameObject != null)
+                {
+                    Vector3 pos = hit.point;
+                    Debug.Log(hit.collider.gameObject);
+                    CreateImpactParticleAtPosition(pos);
+                }
+                if (hit.collider.gameObject.CompareTag("Target"))
+                {
+                    Debug.Log("target");
+                }
             }
-            CreateParticle();
+
         }
     }
 
-    private void OnAim()
+    void CreateImpactParticleAtPosition(Vector3 position)
     {
-        //gameObj.position = 
+        var setParticle = fireParticle.emission;
+        fireParticle.transform.position = position;
+        ParticleSystem.EmissionModule em = fireParticle.emission;
+        em.SetBurst(0, new ParticleSystem.Burst(0.0f, 1, 1, 0.01f));
+        fireParticle.Play();
     }
 
-    private void CreateParticle()
-    {
-        fireParticle.Play();
-        fireParticle.Stop();
-    }
 }
