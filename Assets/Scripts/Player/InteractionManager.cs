a2986498bd12719c3aed786dc43aca45b3a29cfb
GGB <rlqja1018@gmail.com>
2024-02-15 21:13:51 +0900

    fix - 엘베 애니메이션 수정
    수정은 했는데 기본적으로 달려있는 Mesh 콜라이더와도 상호작용이 가능하긴 함

--- Assets/Scripts/Player/InteractionManager.cs
+++ Assets/Scripts/Player/InteractionManager.cs
@@ -41,7 +41,7 @@ public class InteractionManager : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
             {
-                if (hit.collider.gameObject.layer != environmentLayer && hit.collider.gameObject != curInteractGameobject)
+                if (hit.collider.gameObject.layer != environmentLayer) //&& hit.collider.gameObject != curInteractGameobject
                 {
                     curInteractGameobject = hit.collider.gameObject;
                     curInteractable = hit.collider.GetComponent<IInteractable>();
